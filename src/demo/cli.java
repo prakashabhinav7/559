c40049e083f3b80370001a7f7893c021775be25d
ABHINAV PRAKASH <prakashabhinav@prakash.student.iastate.edu>
2017-02-14 11:49:22 -0600

    Public private keys added

--- src/demo/cli.java
+++ src/demo/cli.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.Reader;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.SecureRandom;
@@ -23,11 +24,6 @@ public class cli {
 		Scanner sc = new Scanner(System.in);//Reading input from the user
 		Socket s = new Socket(s_ip,port);//Create client socket - Takes IP address and the port number of the machine and the application on the machine to connect to
 		System.out.println("Connected to server:" + s_ip+ "\nTalking on port:" +  port);
-
-		SecureRandom r_A = new SecureRandom(); //create a 128-bit random number r_A
-		byte[] ivInBytes = new byte[16];
-		r_A.nextBytes(ivInBytes);   
-		System.out.println(ivInBytes);
 		
 		
 		//-------------------------------------------------------------//
@@ -37,18 +33,29 @@ public class cli {
 		
 //		String file = args[2];
 //		Reader r = new FileReader(args[2]);
-		Scanner input = new Scanner(new File(args[2]));
+		Scanner input = new Scanner(new File(args[6]));
 
 //		while (input.hasNextLine())
 //		{
 //		   System.out.println(input.nextLine());
 //		}
 //		
+		
+//		Random number generation
+		SecureRandom sr = new SecureRandom();
+		byte[] ivInBytes = new byte[16];
+		sr.nextBytes(ivInBytes); 
+		
+		
+		BigInteger r_A = new BigInteger(ivInBytes); //storing the 128 bits
+		System.out.println(r_A.bitLength()); 
+		
+		
 		Scanner sc1 = new Scanner(s.getInputStream());
 		System.out.println("\nEnter the number:");
 		number = sc.nextInt();
 		
-		PrintStream p =new PrintStream(s.getOutputStream()); //pushing the number to the server
+		PrintStream p = new PrintStream(s.getOutputStream()); //pushing the number to the server
 		p.println(number);
 		
 		temp=sc1.nextInt();
---
+++ src/demo/sec.java
@@ -0,0 +1,99 @@
+package demo;
+ 
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+ 
+public class sec {
+ 
+	public static void main(String args[]) {
+		sec sec = new sec();
+		try {
+			String path = "";
+ 
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
+			kpg.initialize(2048, r_A); //specify the length of key
+			KeyPair kp = kpg.generateKeyPair( );
+			
+			
+			PublicKey keyPublic = kp.getPublic( );
+			PrivateKey keyPrivate = kp.getPrivate( ); 
+ 
+			System.out.println("Generated Key Pair");
+			sec.dumpKeyPair(generatedKeyPair);
+			sec.SaveKeyPair(path, generatedKeyPair);
+ 
+			KeyPair loadedKeyPair = sec.LoadKeyPair(path, "DSA");
+			System.out.println("Loaded Key Pair");
+			sec.dumpKeyPair(loadedKeyPair);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+ 
+	private void dumpKeyPair(KeyPair keyPair) {
+		PublicKey pub = keyPair.getPublic();
+		System.out.println("Public Key: " + getHexString(pub.getEncoded()));
+ 
+		PrivateKey priv = keyPair.getPrivate();
+		System.out.println("Private Key: " + getHexString(priv.getEncoded()));
+	}
+ 
+	private String getHexString(byte[] b) {
+		String result = "";
+		for (int i = 0; i < b.length; i++) {
+			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+		}
+		return result;
+	}
+ 
+	public void SaveKeyPair(String path, KeyPair keyPair) throws IOException {
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+ 
+		// Store Public Key.
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+				publicKey.getEncoded());
+		FileOutputStream fos = new FileOutputStream(path + "public.key");
+		fos.write(x509EncodedKeySpec.getEncoded());
+		fos.close();
+ 
+		// Store Private Key.
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+				privateKey.getEncoded());
+		fos = new FileOutputStream(path + "private.key");
+		fos.write(pkcs8EncodedKeySpec.getEncoded());
+		fos.close();
+	}
+ 
+	public KeyPair LoadKeyPair(String path, String algorithm)
+			throws IOException, NoSuchAlgorithmException,
+			InvalidKeySpecException {
+		// Read Public Key.
+		File filePublicKey = new File(path + "public.key");
+		FileInputStream fis = new FileInputStream(path + "public.key");
+		byte[] encodedPublicKey = new byte[(int) filePublicKey.length()];
+		fis.read(encodedPublicKey);
+		fis.close();
+ 
+		// Read Private Key.
+		File filePrivateKey = new File(path + "private.key");
+		fis = new FileInputStream(path + "private.key");
+		byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+		fis.read(encodedPrivateKey);
+		fis.close();
+ 
+		// Generate KeyPair.
+		KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+		X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(
+				encodedPublicKey);
+		PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+ 
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(
+				encodedPrivateKey);
+		PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+ 
+		return new KeyPair(publicKey, privateKey);
+	}
+}
